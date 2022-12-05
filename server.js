3030365b41daa9c9bff40084896c5817ac6be5f9
msemerni <msemerni@gmail.com>
2022-12-05 10:08:26 +0200

    continue

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-// mariaDB:
-// user: "mv"
-// password: "MyNewPass5!"
-
-// kupislona:
-// elena33
-// 123
-//////////////////////////////////////////////////////////////////
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
@@ -16,22 +8,34 @@ const { hash, compare } = require('bcrypt');
 const upload = multer({ dest: './public/upload/' });
 const Sequelize = require('sequelize');
 const { Op } = require("sequelize");
+
 const APP_NAME = 'KupiSlona';
 const JWT_SALT = 'JwT_SaLt82&';
 const PORT = 4000;
 
-function jwtCheck(req) {
-  if (req.headers.authorization) {
-    const token = req.headers.authorization.slice(7)
-    try {
-      return verify(token, JWT_SALT)
-    }catch (e) {
-      throw new Error(e);
-    }
+// mariaDB:
+// user: "mv"
+// password: "MyNewPass5!"
+
+// elena33
+// 123
+
+//////////////////////////////////////////////////////////////////
+function jwtCheck(req){
+  if (req.headers.authorization){
+      const token = req.headers.authorization.slice(7)
+      try {
+          return verify(token, JWT_SALT)
+      }
+      catch(e){
+        console.log("ERROR: ", e);
+      }
   }
 }
 
+// const { now } = require('sequelize/types/utils');
 const sequelize = new Sequelize('mysql://mv:MyNewPass5!@127.0.0.1/slon');
+
 
 const getModels = userId => {
 
@@ -42,6 +46,7 @@ const getModels = userId => {
       return this.getImages();
     }
     get user() {
+    // get owner() {
       return this.getUser()
     }
   }
@@ -52,14 +57,29 @@ const getModels = userId => {
     description: Sequelize.STRING,
     price: Sequelize.INTEGER,
     address: Sequelize.STRING,
-  },
-    {
-      sequelize, modelName: 'ad'
-    })
+    },
+    { 
+      // defaultScope: {
+      //   where: {
+      //     deleted: false
+      //   }
+      // },
+
+      scopes: {
+        deleted: {
+          where: {
+            deleted: true
+          }
+        },
+      },
+      
+      sequelize, modelName: 'ad' })
+
+  // // sequelize.sync();
 
   class User extends Sequelize.Model {
     get avatar() {
-      return this.getAvatars({ order: [["id", "DESC"]], limit: 1 });
+      return this.getAvatars({ order:[["id","DESC"]], limit : 1 });
     }
   }
 
@@ -77,7 +97,6 @@ const getModels = userId => {
   class Image extends Sequelize.Model {
 
   }
-
   Image.init({
     originalname: Sequelize.STRING,
     mimetype: Sequelize.STRING,
@@ -87,24 +106,23 @@ const getModels = userId => {
   }, { sequelize, modelName: 'image' })
 
   Image.belongsTo(User, { as: 'avatar' })
-  User.hasMany(Image, {
+  User.hasMany(Image, { 
     as: 'avatars',
-    foreignKey: 'avatarId'
-  })
+    foreignKey: 'avatarId' })
 
   Image.belongsTo(User)
   User.hasMany(Image)
 
   Image.belongsTo(Ad)
-  Ad.hasMany(Image, {
-    foreignKey: 'adId'
-  })
+  Ad.hasMany(Image, { 
+    foreignKey: 'adId' })
+
+// console.log(User.prototype);
 
   return { Ad, User, Image }
 }
-
+//////////////////////////////////////////
 class GlobalUser extends Sequelize.Model {
-
 }
 
 GlobalUser.init({
@@ -113,7 +131,8 @@ GlobalUser.init({
   nick: Sequelize.STRING,
   phones: Sequelize.STRING,
   address: Sequelize.STRING,
-}, { sequelize, modelName: 'user' })
+}, {sequelize, modelName: 'user'})
+
 
 const schema = buildSchema(`
 type Query{
@@ -130,6 +149,7 @@ type Mutation{
   deleteAd(id: ID): Ad
   createUser(login: String!, password: String!): User
   userUpdate(myProfile: UserInput): User
+  
 }
 
 type Ad{
@@ -157,6 +177,7 @@ input AdInput {
   address: String
   
   images: [ID]
+  
 }
 
 type User {
@@ -201,6 +222,7 @@ input ImageInput {
 }
 `)
 
+
 const rootValue = {
 
   async login({ login, password }) {
@@ -209,6 +231,7 @@ const rootValue = {
     }
 
     const user = await GlobalUser.findOne({ where: { login } })
+    // console.log("u_s_e_r", user);
     if (!user) {
       throw new Error("User not found");
     }
@@ -235,21 +258,21 @@ const rootValue = {
 
   async userUpdate({ myProfile }, { thisUser, models: { Image, User } }) {
     if (thisUser) {
-      thisUser.id = thisUser.id;
-      thisUser.login = myProfile.login;
-      thisUser.nick = myProfile.nick;
-      thisUser.phones = myProfile.phones;
-      thisUser.address = myProfile.address;
-      // thisUser = myProfile; ???????????????????????????????
-      await thisUser.save();
-      await thisUser.addAvatar(myProfile.avatar);
-      return thisUser;
+          thisUser.id = thisUser.id;
+          thisUser.login = myProfile.login;
+          thisUser.nick = myProfile.nick;
+          thisUser.phones = myProfile.phones;
+          thisUser.address = myProfile.address;
+          await thisUser.save();
+          await thisUser.addAvatar(myProfile.avatar);
+          return thisUser;
     }
     throw new Error("Unauthorized user");
   },
 
   async getUser({ id }, { thisUser, models: { User } }) {
     if (thisUser) {
+      // console.log("THIS USER: ", thisUser);
       return await User.findByPk(id);
     }
     throw new Error("Unauthorized user");
@@ -257,23 +280,28 @@ const rootValue = {
 
   async getAds({ settings }, { thisUser, models: { Ad } }) {
     if (thisUser) {
-      const { order, offset, limit } = JSON.parse(settings)
-      return await Ad.findAll({ order, offset, limit });
+      const {order, offset, limit } = JSON.parse(settings)
+      return await Ad.findAll({ order, offset, limit});
     }
     throw new Error("Unauthorized user");
   },
 
   async AdFindOne({ id }, { thisUser, models: { User, Ad } }) {
     if (thisUser) {
-      const ad = await Ad.findByPk(id);
+      // console.log("АйДи", id);
+
+      let ad = await Ad.findByPk(id);
+      console.log(ad);
       return ad;
+
     }
     throw new Error("Unauthorized user");
   },
 
   async userAdFind({ id }, { thisUser, models: { User, Ad } }) {
     if (thisUser) {
-      let ads = await Ad.findAll({ order: [["id", "DESC"]], where: { UserId: id } });
+      console.log(id);
+      let ads = await Ad.findAll({ order:[["id","DESC"]], where: { UserId: id } });
       return ads;
     }
     throw new Error("Unauthorized user");
@@ -281,24 +309,27 @@ const rootValue = {
 
   async AdSearch({ queryString }, { thisUser, models: { User, Ad } }) {
     if (thisUser) {
-      let ads = await Ad.findAll({
-        where: {
-          [Op.or]: [
-            { title: { [Op.like]: `%${queryString}%` } },
-            { description: { [Op.like]: `%${queryString}%` } },
-          ]
-        }
-      });
+      console.log(queryString);
+      let ads = await Ad.findAll({ where: {
+        [Op.or]: [
+          {title: { [Op.like]: `%${queryString}%` }},
+          {description: { [Op.like]: `%${queryString}%` }},
+        ]
+      }});
 
+      console.log(ads);
       return ads;
     }
     throw new Error("Unauthorized user");
   },
 
+  
   async deleteAd({ id }, { thisUser, models: { User, Ad } }) {
     if (thisUser) {
       try {
         let ad = await Ad.findByPk(id);
+        // let user = await User.findByPk(thisUser.id);
+        // await user.removeAd(ad);
         await Ad.destroy(
           { where: { id } })
 
@@ -307,35 +338,53 @@ const rootValue = {
       } catch (error) {
         throw new Error("Something went wrong");
       }
+
     }
     throw new Error("Unauthorized user");
   },
 
   async upsertAd({ ad }, { thisUser, models: { User, Ad } }) {
     if (thisUser) {
+      console.log("'АД:::': ", ad);
+
       // if (await thisUser.hasImages(ad.imageIds)) {
-      let dbAd;
-      if (ad.id) {
-        dbAd = await Ad.findByPk(ad.id);
-        dbAd.title = ad.title;
-        dbAd.tags = ad.tags;
-        dbAd.price = ad.price;
-        dbAd.description = ad.description;
-        dbAd.address = ad.address;
-        // dbAd = ad; ???????????????????????????????
-        await dbAd.save();
+        let dbAd;
+        if (ad.id) {
+          dbAd = await Ad.findByPk(ad.id);
 
-        // await dbAd.removeImage(dbAd.images);
-        // await dbAd.removeImage(dbAd.adId); // ???????????????????????????????????????
+          dbAd.title = ad.title;
+          dbAd.tags = ad.tags;
+          dbAd.price = ad.price;
+          dbAd.description = ad.description;
+          dbAd.address = ad.address;
+          // dbAd = ad; ?????
+          await dbAd.save();
 
-        await dbAd.addImage(ad.images);
-        return dbAd;
-      }
-      else {
-        dbAd = await thisUser.createAd(ad);
-        await dbAd.addImage(ad.images);
-        return dbAd;
-      }
+          
+          // await dbAd.images.destroy({where: {id}})
+          // await dbAd.images.destroy();
+          console.log("'DBAD': ", dbAd);
+          const DBAD_IMG = await dbAd.images;
+          console.log("'DBAD_IMG': ", DBAD_IMG);
+          // await dbAd.removeImage(dbAd.images);
+          // await dbAd.removeImage(dbAd.adId); // ???????????????????????????????????????
+
+          await dbAd.addImage(ad.images);
+          return dbAd;
+        }
+        else {
+          // console.log("THIS____USER: ", thisUser);
+          // console.log("'DBAD': ", dbAd);
+
+          // if (ad.tags.length === 0) {
+          // }
+
+          dbAd = await thisUser.createAd(ad);
+          await dbAd.addImage(ad.images);
+          return dbAd;
+        }
+
+        // return dbAd;
       // }
       // throw new Error("Not user's image");
     }
@@ -344,18 +393,21 @@ const rootValue = {
 
 }
 
+
 const app = express();
-
 app.use(express.static('public'));
-
 app.use(bodyParser.json());
+
 
 app.use('/graphql', graphqlHTTP(async (req, res) => {
   const decodedUser = jwtCheck(req);
+  // console.log("DECODED USER: ", decodedUser);
 
   if (decodedUser) {
     const models = getModels(decodedUser.id);
     const thisUser = await models.User.findByPk(decodedUser.id)
+    // console.log("MODELS: ", models) // { Ad: ad, User: user, Image: image }
+    // console.log("ThIs USER: ", thisUser) //user {dataValues: {id: 1,login: 'vasya'.....
     return {
       schema,
       rootValue,
@@ -371,20 +423,28 @@ app.use('/graphql', graphqlHTTP(async (req, res) => {
   }
 }))
 
+
 app.post('/upload', upload.single('dropZone'), async (req, res) => {
-  const decodedUser = jwtCheck(req)
-  const models = getModels(decodedUser.id);
+// app.post('/upload', upload.array('file'), async (req, res) => {    ???????????????
 
-  if (decodedUser) {
-    const { originalname, mimetype, filename, size, path } = req.file
-    const url = path.slice(7);
-    const image = await models.Image.create({ originalname, mimetype, filename, size, url, userId: decodedUser.id })
+  // console.log("REQ_FILE: ", req.file);
 
-    res.status(201).end(JSON.stringify(image))
+    const decodedUser = jwtCheck(req)
+    const models = getModels(decodedUser.id);
+
+    if (decodedUser) {
+      const {originalname, mimetype, filename, size, path} = req.file
+      // console.log("P_A_T_H: ", path);
+      url = path.slice(7);
+      const image = await models.Image.create({originalname, mimetype, filename, size, url, userId: decodedUser.id})
+      // console.log("I_M_A_G_E: ", image);
+
+      res.status(201).end(JSON.stringify(image))
+    }
+    else {
+      res.status(403).end('Unauthorized file upload prohibited')
   }
-  else {
-    res.status(403).end('Unauthorized file upload prohibited')
-  }
+    
 });
 
 app.listen(PORT, () => {
@@ -400,64 +460,107 @@ app.listen(PORT, () => {
 
 
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  ; (async () => {
-    // await sequelize.sync({ force: true }) // пересоздаст таблицы
-    // await sequelize.sync({ alter: true }) // приводит таблицу в соответствие с моделью
-    ////// await sequelize.sync()
-    // const vasya1 = await GlobalUser.create({ 
-    //   login: 'vasya', 
-    //   password: await hash('123', 10),
-    // });
-    // const petya2 = await GlobalUser.create({ 
-    //   login: 'petya', 
-    //   password: await hash('123', 10),
-    // });
-    // const elena = await GlobalUser.create({ 
-    //   login: 'elena33', 
-    //   password: await hash('123', 10),
-    // });
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+; (async () => {
+  // await sequelize.sync({ force: true }) // пересоздаст таблицы
+  // await sequelize.sync({ alter: true }) // приводит таблицу в соответствие с моделью
+  ////// await sequelize.sync()
+  // const vasya1 = await GlobalUser.create({ 
+  //   login: 'vasya', 
+  //   password: await hash('123', 10),
+  // });
+  // const petya2 = await GlobalUser.create({ 
+  //   login: 'petya', 
+  //   password: await hash('123', 10),
+  // });
+  // const elena = await GlobalUser.create({ 
+  //   login: 'elena33', 
+  //   password: await hash('123', 10),
+  // });
 
-    // const models1 = getModels(1);
-    // const models2 = getModels(2);
-    // const models3 = getModels(3);
+  // const models1 = getModels(1);
+  // const models2 = getModels(2);
+  // const models3 = getModels(3);
 
-    // await models1.Ad.create({
-    //   title: 'Продам телефон',
-    //   tags: "Electronics",
-    //   description: "Xiaomi MI9 ",
-    //   price: 700,
-    //   address: "Харьков",
-    //   userId: 1
-    // })
+  // await models1.Ad.create({
+  //   title: 'Продам телефон',
+  //   tags: "Electronics",
+  //   description: "Xiaomi MI9 ",
+  //   price: 700,
+  //   address: "Харьков",
+  //   userId: 1
+  // })
 
-    // await models2.Ad.create({
-    //   title: 'Продам диван',
-    //   tags: "House and garden",
-    //   description: "Новый диван",
-    //   price: 2000,
-    //   address: "Киев",
-    //   userId: 2
-    // });
+  // await models2.Ad.create({
+  //   title: 'Продам диван',
+  //   tags: "House and garden",
+  //   description: "Новый диван",
+  //   price: 2000,
+  //   address: "Киев",
+  //   userId: 2
+  // });
 
-    // await models3.Ad.create({
-    //    title: 'Частный дом',
-    //    tags: "House and garden",
-    //    description: "Частный дом 100 квадратов",
-    //    price: 15000,
-    //    address: "Харьков",
-    //    userId: 3
-    //    });
+  // await models3.Ad.create({
+  //    title: 'Частный дом',
+  //    tags: "House and garden",
+  //    description: "Частный дом 100 квадратов",
+  //    price: 15000,
+  //    address: "Харьков",
+  //    userId: 3
+  //    });
 
-    // await models3.Ad.create({
-    //    title: 'Дом',
-    //    tags: "House and garden",
-    //    description: "продам домик",
-    //    price: 8000,
-    //    address: "Харьков",
-    //    userId: 3
-    //    });
+  // await models3.Ad.create({
+  //    title: 'Дом',
+  //    tags: "House and garden",
+  //    description: "продам домик",
+  //    price: 8000,
+  //    address: "Харьков",
+  //    userId: 3
+  //    });
 
-  })//();
+})//();
+
+
+
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//   async addAd({ ad }, {thisUser, models: {User}}) {
+//     if (thisUser) {
+//       if (await thisUser.hasImages(ad.imageIds)) {
+//         const newAd = await thisUser.createAd(ad);
+//         await newAd.setImages(ad.imageIds);
+//         return newAd;
+//       }
+//       throw new Error("Not user's image");
+//     }
+
+//     throw new Error("Unauthorized user");
+
+//   },
+
+//   async editAd({ ad }, { thisUser, models: {Ad} }){
+//     if (thisUser && await thisUser.hasImages(ad.imageIds || [])){
+//         let dbAd = await Ad.findByPk(ad.id);
+
+//         // console.log("'DBAD': ", dbAd);
+//         // console.log("'ad': ", ad);
+
+//         dbAd.title = ad.title;
+//         dbAd.tags = ad.tags;
+//         dbAd.price = ad.price;
+//         dbAd.description = ad.description;
+//         dbAd.address = ad.address;
+
+//         // dbAd = ad;
+
+//         await dbAd.save();
+//         await dbAd.setImages(ad.imageIds);
+
+//         return dbAd;
+//     }
+// },
+
+// src="http://localhost:4000/upload/6bd4bbc95702d827b634c7ab9a56dfb8"
