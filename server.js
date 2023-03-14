f2ecffa7dbda70e73d8d4155d96b6eaa63ef49f9
msemerni <msemerni@gmail.com>
2023-03-14 21:04:57 +0200

    update

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const { Op } = require("sequelize");
 
 const APP_NAME = 'KupiSlona';
 const JWT_SALT = 'JwT_SaLt82&';
-// const PORT = 4000;
-const PORT = 443;
+const PORT = 4000;
+// const PORT = 443;
 
 // mariaDB:
 // user: "mv"
